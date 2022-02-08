708e2a245b3d8a580488992d36cc6e1bc329db70
Nikolai Pustovoit <nikolaikop@gmail.com>
2022-02-08 11:29:50 -0700

    Arrays V1

---
+++ task.js
@@ -0,0 +1,14 @@
+function compareArrays(arr1, arr2) {
+  let result = (arr1.length === arr2.length) && (arr1.every((item, index) => item === arr2[item, index]));
+  return result
+}
+compareArrays()
+
+function advancedFilter(arr) {
+  let resultArr;
+
+  // Ваш код
+  resultArr = arr.filter(item => item > 0 && item % 3 === 0).map(item => item *10);
+
+  return resultArr; // array
+}
